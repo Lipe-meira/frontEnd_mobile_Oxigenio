46f4ab708c143ee3d109b85871339d44d6ec35e4
Lipe-meira <felipemeira018@gmail.com>
2026-07-09 14:03:10 -0300

    fix: add new episodes

--- src/data/episodios.js
+++ src/data/episodios.js
@@ -12,7 +12,7 @@ const episodios = [
       'Neste episódio, recebemos Ana Júlia Krensiglova, coordenadora da Associação Náutica de Itajaí (ANI), e Adriano Marenzi, professor e biólogo marinho. Vamos explorar a relação de Itajaí com o mar, as ações da ANI no fortalecimento da cultura náutica local e as iniciativas ambientais para preservar os costões do Litoral Norte catarinense.',
     participantes: ['Oxigênio Central de Podcast', 'Ana Júlia Krensiglova, Adriano Marenzi'],
     link: 'https://open.spotify.com/episode/00T3Rch8aWrUysBnMQDp7M',
-    destaque: true,
+    destaque: false,
     demonstrativo: true,
   },
   {
@@ -88,6 +88,101 @@ const episodios = [
     participantes: ['Oxigênio Central de Podcast', 'Dona Bernadete Berto'],
     link: 'https://open.spotify.com/episode/4Gqy90CsPclC4DhB5HCQJg',
     destaque: false,
+    demonstrativo: true,
+  },
+  {
+    id: '7',
+    titulo: 'Projeto Rondon: impactos da extensão universitária sob o olhar da profa. Ednéia Casagranda Bueno',
+    categoria: 'Cultura',
+    ano: '2025',
+    duracao: '24 min',
+    descricaoCurta:
+      'Projeto Rondon: impactos da extensão universitária sob o olhar da profa. Ednéia Casagranda Bueno',
+    descricaoCompleta:
+      'O conhecimento é capaz de transformar vidas. Esta é uma das missões dos universitários que participam do Projeto Rondon, do Ministério da Defesa, que leva capacitação e partilha de vivências a diversas regiões do Brasil. Para falar sobre o impacto dos projetos de extensão universitária na vida das pessoas, o Oxigênio - Central de Podcasts recebeu neste episódio a professora pós-doutorada Ednéia Casagranda Bueno, coordenadora do Conjunto A da Universidade do Vale do Itajaí (Univali) na Operação Amazonas 2025. Ouça e entenda um pouco mais sobre este lindo projeto, que desde os anos 70 dá uma “lição de brasilidades” a universitários de todo o país.',
+    participantes: ['Oxigênio Central de Podcast', 'Ednéia Casagranda Bueno'],
+    link: 'https://open.spotify.com/episode/32J89bwfBDnoDtOyhT81ma',
+    destaque: false,
+    demonstrativo: true,
+  },
+
+   {
+    id: '8',
+    titulo: '“Se despir”: lições de vida e de cidadania, com a universitária Manuela de Pádua Portero',
+    categoria: 'Cidadania',
+    ano: '2025',
+    duracao: '35 min',
+    descricaoCurta:
+      '“Se despir”: lições de vida e de cidadania, com a universitária Manuela de Pádua Portero',
+    descricaoCompleta:
+      'Se você é capaz de esquecer o seu “vocabulário”, você tem asas para voar. Em outras palavras, “se despir” dos preceitos. Esta é uma das mensagens que a universitária Manuela de Pádua Portero, do curso de Psicologia da Universidade do Vale do Itajaí (Univali), esclareceu neste episódio do Oxigênio - Central de Podcasts sobre a participação na Operação Sentinelas Avançadas II, do Projeto Rondon. Ouça e conheça algumas das experiências transformadoras que marcaram a vida de Manuela e seus colegas.',
+    participantes: ['Oxigênio Central de Podcast', 'Manuela de Pádua Portero'],
+    link: 'https://open.spotify.com/episode/5ZxNtNdFX53gVql64Mapzq',
+    destaque: false,
+    demonstrativo: true,
+  },
+
+   {
+    id: '9',
+    titulo: 'Bem-estar do verde: dicas para o cultivo de hortaliças, temperos e plantas, com Renê Artur Ferreira',
+    categoria: 'Meio Ambiente',
+    ano: '2025',
+    duracao: '40 min',
+    descricaoCurta:
+      'Bem-estar do verde: dicas para o cultivo de hortaliças, temperos e plantas, com Renê Artur Ferreira',
+    descricaoCompleta:
+      'O cultivo de plantas, hortaliças e temperos traz não apenas benefícios para a saúde física, mas também para a saúde mental. Embora a prática seja recomendada por especialistas, muitas pessoas não possuem a expertise para cultivar um espaço verde dentro de casa ou do apartamento, por exemplo. Neste episódio do Oxigênio Central de Podcast, o engenheiro agrônomo e responsável pelo Horto Medicinal da Univali, professor Renê Artur Ferreira, nos dá dicas sobre o cultivo de plantas em ambientes internos. Ouça e comece hoje mesmo!',
+    participantes: ['Oxigênio Central de Podcast', 'Renê Artur Ferreira'],
+    link: 'https://open.spotify.com/episode/24ab3VeZRsPvMLyVKfJa9n',
+    destaque: false,
+    demonstrativo: true,
+  },
+
+   {
+    id: '10',
+    titulo: 'Entre Linhas e Agulhas: Rose Bittencourt e os 50 anos costurando histórias!',
+    categoria: 'Cultura',
+    ano: '2025',
+    duracao: '35 min',
+    descricaoCurta:
+      'Entre Linhas e Agulhas: Rose Bittencourt e os 50 anos costurando histórias!',
+    descricaoCompleta:
+      'A costura é uma arte, carrega histórias, e passa de geração em geração conectando pessoas. Neste episódio do podcast, os estudantes do curso de Design de Moda entrevistam Rose Bittencourt, costureira com mais de 50 anos de trajetória. Ao longo da conversa, Rose compartilha sua experiência na costura e de vida, as transformações que pode viver na moda e a importância do trabalho manual, compartilhou também algumas curiosidades sobre esses anos de carreira. Um episódio para quem valoriza o feito à mão, a sabedoria de quem viveu a moda de perto e a inspiração de uma vida dedicada à costura. Aperte o play e conheça essa história!',
+    participantes: ['Oxigênio Central de Podcast', 'Rose Bittencourt'],
+    link: 'https://open.spotify.com/episode/1cdgzEqN7u7kb562vYt6Kd',
+    destaque: false,
+    demonstrativo: true,
+  },
+
+     {
+    id: '11',
+    titulo: 'Costura que cura: a arte de costurar como aliada da saúde mental, com Jaqueline Tesch e Matheus Oliver',
+    categoria: 'Saúde e Bem-estar',
+    ano: '2025',
+    duracao: '58 min',
+    descricaoCurta:
+      'Costura que cura: a arte de costurar como aliada da saúde mental, com Jaqueline Tesch e Matheus Oliver',
+    descricaoCompleta:
+      'Costura que cura: a arte de costurar como aliada da saúde mental, com Jaqueline Tesch e Matheus OliverA prática da costura vai muito além da criação de peças e roupas: ela também pode ser uma poderosa aliada no cuidado com a saúde emocional. Em meio a tecidos, linhas e agulhas, muitas pessoas encontram um espaço de silêncio, foco e acolhimento. Neste episódio do Oxigênio Central Podcast, conversamos sobre como a costura pode funcionar como uma forma de terapia e autocuidado. Ouça e descubra os benefícios de costurar o próprio bem-estar!',
+    participantes: ['Oxigênio Central de Podcast', 'Jaqueline Tesch e Matheus Oliver'],
+    link: 'https://open.spotify.com/episode/43CqWVYUXCdbPrClqGDwm9',
+    destaque: false,
+    demonstrativo: true,
+  },
+
+   {
+    id: '12',
+    titulo: 'O papel dos congressos na formação acadêmica de estudantes de comunicação - Especial Intercom Sul 2026',
+    categoria: 'Cultura',
+    ano: '2026',
+    duracao: '8 min',
+    descricaoCurta:
+      'O papel dos congressos na formação acadêmica de estudantes de comunicação - Especial Intercom Sul 2026',
+    descricaoCompleta:
+      'Neste episódio você acompanhará algumas entrevistas feitas pelos acadêmicos de jornalismo da Univali no Intercom Sul 2026, realizado entre os dias 4 e 6 de junho em Toledo, no Oeste do Paraná. Além de praticar as técnicas de entrevista, reportagem e comunicação, os acadêmicos Caio da Costa, Júlia Kato e Luiza Kramer descobriram um pouco mais sobre a importância dos congressos para o desenvolvimento de novos conhecimentos científicos e conexões fundamentais ao longo da carreira, seja na área da comunicação ou fora dela. Nosso objetivo com este episódio do Oxigênio é dar visibilidade ao trabalho dos estudantes e estimular você, que está nos ouvindo, a procurar novos horizontes, novos voos dentro da área em que está atuando! ',
+    participantes: ['Oxigênio Central de Podcast', 'Caio da Costa, Júlia Kato e Luiza Kramer'],
+    link: 'https://open.spotify.com/episode/6FAjW9rBmTRlBubJwMqqDB',
+    destaque: true,
     demonstrativo: true,
   },
 ];
